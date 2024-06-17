981a31b649245e9041c417cbe871149799eaa848
Frederic Lehmann <frederic.lehmann@bluewin.ch>
2024-06-17 22:12:54 +0200

    add shadcn dropdown component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,18 @@
 export { Button, buttonVariants } from './button';
+export {
+	DropdownMenu,
+	DropdownMenuTrigger,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuCheckboxItem,
+	DropdownMenuRadioItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuGroup,
+	DropdownMenuPortal,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuRadioGroup
+} from './dropdown-menu';
