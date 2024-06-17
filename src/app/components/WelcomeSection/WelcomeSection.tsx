4e0fd74d86f6c6d29be3f5076587964a99e2dc8d
Frederic Lehmann <frederic.lehmann@bluewin.ch>
2024-06-17 21:13:40 +0200

    add first custom section

---
+++ src/app/components/WelcomeSection/WelcomeSection.tsx
@@ -0,0 +1,11 @@
+export default function WelcomeSection() {
+	return (
+		<section className="py-12 md:py-24 lg:py-32 container mx-auto">
+			<div className="flex flex-col justify-center space-y-3">
+				<span className="text-lg md:text-xl">Welcome to the personal website of</span>
+				<h1 className="text-3xl md:text-5xl font-bold">Frederic Lehmann </h1>
+				<div></div>
+			</div>
+		</section>
+	);
+}
