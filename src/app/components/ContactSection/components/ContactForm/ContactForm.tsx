5ca062327f6631fef0cdf4c6cb3ba538c7d7e569
Frederic Lehmann <frederic.lehmann@bluewin.ch>
2024-06-24 22:11:52 +0200

    add feedback on status

--- src/app/components/ContactSection/components/ContactForm/ContactForm.tsx
+++ src/app/components/ContactSection/components/ContactForm/ContactForm.tsx
@@ -1,14 +1,21 @@
 'use client';
 
+import { Loader2Icon, SendIcon } from 'lucide-react';
+import { useState } from 'react';
+
 import { Button, Input, Label, Textarea } from '@/components/ui';
 
 import sendEmail from './sendEmail';
 
 export default function ContactForm() {
+	const [sending, setSending] = useState(false);
+	const [success, setSuccess] = useState(false);
+
 	async function submitForm(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
+		setSending(true);
 
-		await sendEmail({
+		const result = await sendEmail({
 			// @ts-ignore
 			name: (e.target as HTMLFormElement).elements['name'].value,
 			// @ts-ignore
@@ -16,6 +23,10 @@ export default function ContactForm() {
 			// @ts-ignore
 			comment: (e.target as HTMLFormElement).elements['comment'].value
 		});
+
+		if (result.success) setSuccess(true);
+
+		setSending(false);
 	}
 
 	return (
@@ -32,8 +43,20 @@ export default function ContactForm() {
 				<Label htmlFor="comment">Comment</Label>
 				<Textarea id="comment" placeholder="What do you wan't me to know" required />
 			</div>
-			<Button type="submit" className="w-full">
-				Send
+			<Button type="submit" className="w-full" disabled={sending || success}>
+				{sending && (
+					<>
+						<Loader2Icon className="w-5 h-5 animate-spin mr-2" />
+						Send
+					</>
+				)}
+				{success && (
+					<>
+						<SendIcon className="w-5 h-5 mr-2" />
+						Sent
+					</>
+				)}
+				{!sending && !success && 'Send'}
 			</Button>
 		</form>
 	);
